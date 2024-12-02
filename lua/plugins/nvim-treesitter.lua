43cc9c47b4242a049177476f6ba849a8dfc6276e
Manh Truong <manhattan@Manhs-MacBook-Pro.local>
2024-12-02 21:12:23 +0700

    more files

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,9 +1,10 @@
 return {
-  'nvim-treesitter/nvim-treesitter', 
+  'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
     local treesitter_config = require("nvim-treesitter.configs")
     treesitter_config.setup({
+      auto_install = true,
       ensure_installed = {"lua", "cpp", "c", "python", "javascript"},
       highlight = { enable = true },
       indent = { enable = true}
