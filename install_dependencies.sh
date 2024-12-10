ae514aa211f0cb166f19eb08fa5be32b4b107c46
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-10 16:52:33 +0700

    asb

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo apt install clangd clangd-format python3-pip
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y clangd clangd-format python3-pip nodejs
 pip3 install --user pyright
