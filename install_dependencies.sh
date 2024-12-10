a42d7d5b47b9e9594b943b6558ac88b08231cf39
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-10 14:40:28 +0700

    add some

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt install stylua
+sudo apt install clangd clangd-format python3-pip
+pip3 install --user pyright
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -46,6 +46,11 @@ return
           }
         },
         defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            ".build",
+            ".cache"
+          },
           mappings = {
             i = {
               ["<cr>"] = function(bufnr)
