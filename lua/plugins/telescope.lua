69ebe6af313a782de81cd29ebd1aad6914b9ea09
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-10 13:51:03 +0700

    tidy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,19 +13,24 @@ return
         function()
           builtin.find_files({ cwd = utils.buffer_dir() })
         end, {})
-      map('n', '<leader>fg', builtin.live_grep, {})
-      map('n', '<leader>fs', builtin.lsp_document_symbols, {})
-      map('n', '<leader>fb', builtin.buffers, {})
-
-      map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
-      map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+      map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+        { desc = "telescope find all files" })
       map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>",
         { desc = "telescope find in current buffer" })
+      map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+      map('n', '<leader>fg', builtin.live_grep, {})
+
+      map('n', '<leader>fs', builtin.lsp_document_symbols, {})
+      map('n', '<leader>fS', builtin.lsp_workspace_symbols, {})
+      map('n', '<leader>fb', builtin.buffers, {})
+      map('n', '<leader>gd', builtin.lsp_definitions, {})
+      map('n', '<leader>gD', builtin.lsp_type_definitions, {})
+      map('n', '<leader>gi', builtin.lsp_implementations, {})
+
+      map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
       map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
       map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
       map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
-      map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-        { desc = "telescope find all files" })
     end
   },
   {
