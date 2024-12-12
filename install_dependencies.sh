406b795fe55532aa600b2aa6a3980db67c386e61
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-12 15:00:52 +0700

    add more

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y clangd clangd-format python3-pip nodejs
+sudo apt install -y clangd clangd-format python3-pip python3-venv nodejs
 pip3 install --user pyright
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,6 +11,9 @@ vim.g.maplocalleader = "\\"
 local map = vim.keymap.set
 
 map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
-map("n", "<C-q>", "<cmd>q<CR>", { desc = "toggle line number" })
+map("n", "<C-q>q", "<cmd>q<CR>", { desc = "Quit buffer" })
+map("n", "<C-q>a", "<cmd>qa<CR>", { desc ="Quit all buffer" })
+map("v", "<C-c>", "\"+y", { desc ="Copy to system clipboard" })
+map("v", "<C-s>", "<cmd>w<CR>", { desc ="save buffer" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
