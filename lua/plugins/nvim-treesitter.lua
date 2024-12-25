8f10d8ba4641ccb9a6139a22a298d6262e7b525d
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-25 17:05:34 +0700

    add bunch of

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,16 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   config = function()
     local treesitter_config = require("nvim-treesitter.configs")
     treesitter_config.setup({
       auto_install = true,
-      ensure_installed = {"lua", "cpp", "c", "python", "javascript"},
+      ensure_installed = { "lua", "cpp", "c", "python", "javascript", "cmake" },
       highlight = { enable = true },
-      indent = { enable = true}
+      indent = { enable = true }
     })
   end
 }
