b89b98a149893abe351e367dd0c85ed364cba1ac
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-26 09:05:47 +0700

    setup codeium

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,34 +12,35 @@ return {
   {
     'hrsh7th/nvim-cmp',
     config = function()
-        local cmp = require'cmp'
-        require("luasnip.loaders.from_vscode").lazy_load()
+      local cmp = require 'cmp'
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-        cmp.setup({
-          snippet = {
-            -- REQUIRED - you must specify a snippet engine
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            end,
-          },
-          window = {
-            completion = cmp.config.window.bordered(),
-            documentation = cmp.config.window.bordered(),
-          },
-          mapping = cmp.mapping.preset.insert({
-           --  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          }),
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' }, -- For luasnip users.
-          }, {
-            { name = 'buffer' },
-          })
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)   -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          --  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'codeium' },
+          { name = 'luasnip' },   -- For luasnip users.
+        }, {
+          { name = 'buffer' },
         })
+      })
     end
   },
 }
