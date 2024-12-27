7feab0872c79117255c5bceb6f08f3e016e52f07
Truong Tuan Manh <manhattan.vn@gmail.com>
2024-12-27 23:20:04 +0700

    setup for Obsidian

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set conceallevel=1")
 vim.cmd("set rnu")
 vim.cmd("set nu")
 -- Make sure to setup `mapleader` and `maplocalleader` before
