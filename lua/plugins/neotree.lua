0d1e7f639814f84020b6ea7f299eab1f4f3ed52b
Truong Tuan Manh <manh.truong@techvico.com>
2024-12-30 16:21:33 +0700

    neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,11 +3,20 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>", {})
+    require("neo-tree").setup({
+      close_if_last_window = true, -- Close Neo-tree if it is the last window,
+      windows = {
+        position = "left",
+      },
+      filesystem = {
+        follow_current_file = true, -- Highlight the current file
+      }
+    })
+    vim.keymap.set('n', '<C-b>', "<cmd>Neotree toggle<CR>", { noremap = true, silent = true })
   end
 }
