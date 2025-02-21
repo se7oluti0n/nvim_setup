c422d3508181218ad740e87e2f6da6d9005f07c8
Truong Tuan Manh <manh.truong@techvico.com>
2025-02-21 13:16:11 +0700

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y stylua clangd clangd-format python3-pip python3-venv nodejs
+sudo apt install -y stylua clangd clang-format python3-pip python3-venv nodejs
 pip3 install --user pyright
+npm i @johnnymorganz/stylua-bin
