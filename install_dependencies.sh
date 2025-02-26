4502884195eb317cab39abe885df33004a2a697d
Truong Tuan Manh <manh.truong@techvico.com>
2025-02-26 13:48:14 +0700

    add avante

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y stylua clangd clang-format python3-pip python3-venv nodejs
+sudo apt install -y luajit clangd clang-format python3-pip python3-venv nodejs
 pip3 install --user pyright
-npm i @johnnymorganz/stylua-bin
+sudo npm i @johnnymorganz/stylua-bin
